faf4fca647379cc37366a223c69406046937f4b4
unknown <wclaybaugh@gmail.com>
2018-11-05 12:25:14 -0500

    corrections to unary function tests; extensions to allow correct pattern for inverse trig functions

--- code/test_unary_functions.py
+++ code/test_unary_functions.py
@@ -1,15 +1,26 @@
 import pytest
 import autodiff as ad
+import math
 
 @pytest.fixture
 def b():
     from autodiff import DualNumber
     b = DualNumber(None,-2/3,{'x':1.2, 'y':9.5, 'z':5})
     return b
+	
+@pytest.fixture
+def binv():
+    from autodiff import DualNumber
+    binv = DualNumber(None,-2/3,{'x':1.2, 'y':9.5, 'z':5})
+    return binv
 
 @pytest.fixture
 def s():
     return .42
+	
+@pytest.fixture
+def sinv():
+    return 1/.42
 
 ###
 # negation
@@ -30,17 +41,22 @@ def test_negation_no_change(b):
 # sin
 ###
 def test_sin_s(s):
+	def value(x):
+		return math.sin(x)
 	output = ad.sin(s)
-	assert output.value == math.sin(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_sin_d(b):
+	def value(x):
+		return math.sin(x)
 	def der(x):
 		return math.cos(x)
 	
 	output = ad.sin(b)
-	assert output.value == math.sin(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_sin_no_change(b):
     b_d = b.derivatives
@@ -52,17 +68,22 @@ def test_sin_no_change(b):
 # cos
 ###
 def test_cos_s(s):
+	def value(x):
+		return math.cos(x)
 	output = ad.cos(s)
-	assert output.value == math.cos(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_cos_d(b):
+	def value(x):
+		return math.cos(x)
 	def der(x):
 		return -math.sin(x)
 	
 	output = ad.cos(b)
-	assert output.value == math.cos(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_cos_no_change(b):
     b_d = b.derivatives
@@ -74,17 +95,22 @@ def test_cos_no_change(b):
 # tan
 ###
 def test_tan_s(s):
+	def value(x):
+		return math.tan(x)
 	output = ad.tan(s)
-	assert output.value == math.tan(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_tan_d(b):
+	def value(x):
+		return math.tan(x)
 	def der(x):
-		return math.sec(x)**2
+		return 1/(math.cos(x)**2)
 	
 	output = ad.tan(b)
-	assert output.value == math.tan(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_tan_no_change(b):
     b_d = b.derivatives
@@ -96,17 +122,22 @@ def test_tan_no_change(b):
 # cot
 ###
 def test_cot_s(s):
+	def value(x):
+		return 1/math.tan(x)
 	output = ad.cot(s)
-	assert output.value == math.cot(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_cot_d(b):
+	def value(x):
+		return 1/math.tan(x)
 	def der(x):
-		return -math.csc(x)**2
+		return -1/(math.sin(x)**2)
 	
 	output = ad.cot(b)
-	assert output.value == math.cot(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_cot_no_change(b):
     b_d = b.derivatives
@@ -118,17 +149,22 @@ def test_cot_no_change(b):
 # sec
 ###
 def test_sec_s(s):
+	def value(x):
+		return 1/math.cos(x)
 	output = ad.sec(s)
-	assert output.value == math.sec(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_sec_d(b):
+	def value(x):
+		return 1/math.cos(x)
 	def der(x):
-		return math.sec(x)*math.tan(x)
+		return 1/math.cos(x)*math.tan(x)
 	
 	output = ad.sec(b)
-	assert output.value == math.sec(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_sec_no_change(b):
     b_d = b.derivatives
@@ -140,17 +176,22 @@ def test_sec_no_change(b):
 # csc
 ###
 def test_csc_s(s):
+	def value(x):
+		return 1/math.sin(x)
 	output = ad.csc(s)
-	assert output.value == math.csc(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_csc_d(b):
+	def value(x):
+		return 1/math.sin(x)
 	def der(x):
-		return -math.csc(x)*math.cot(x)
+		return -1/math.sin(x)*1/math.tan(x)
 	
 	output = ad.csc(b)
-	assert output.value == math.csc(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_csc_no_change(b):
     b_d = b.derivatives
@@ -162,17 +203,22 @@ def test_csc_no_change(b):
 # arcsin
 ###
 def test_arcsin_s(s):
+	def value(x):
+		return math.asin(x)
 	output = ad.arcsin(s)
-	assert output.value == math.arcsin(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_arcsin_d(b):
+	def value(x):
+		return math.asin(x)
 	def der(x):
 		return 1/math.sqrt(1-x**2)
 	
 	output = ad.arcsin(b)
-	assert output.value == math.arcsin(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_arcsin_no_change(b):
     b_d = b.derivatives
@@ -184,17 +230,22 @@ def test_arcsin_no_change(b):
 # arccos
 ###
 def test_arccos_s(s):
+	def value(x):
+		return math.acos(x)
 	output = ad.arccos(s)
-	assert output.value == math.arccos(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_arccos_d(b):
+	def value(x):
+		return math.acos(x)
 	def der(x):
 		return -1/math.sqrt(1-x**2)
 	
 	output = ad.arccos(b)
-	assert output.value == math.arccos(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_arccos_no_change(b):
     b_d = b.derivatives
@@ -206,17 +257,22 @@ def test_arccos_no_change(b):
 # arctan
 ###
 def test_arctan_s(s):
+	def value(x):
+		return math.atan(x)
 	output = ad.arctan(s)
-	assert output.value == math.arctan(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+	assert output.value == value(s)
+	assert output.derivatives == {}
 
 def test_arctan_d(b):
+	def value(x):
+		return math.atan(x)
 	def der(x):
 		return 1/(1+x**2)
 	
 	output = ad.arctan(b)
-	assert output.value == math.arctan(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	assert output.value == value(b.value)
+	coef = der(b.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
 def test_arctan_no_change(b):
     b_d = b.derivatives
@@ -227,65 +283,80 @@ def test_arctan_no_change(b):
 ###
 # arccot
 ###
-def test_arccot_s(s):
-	output = ad.arccot(s)
-	assert output.value == math.arccot(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+def test_arccot_s(sinv):
+	def value(x):
+		return math.atan(1/x)
+	output = ad.arccot(sinv)
+	assert output.value == value(sinv)
+	assert output.derivatives == {}
 
-def test_arccot_d(b):
+def test_arccot_d(binv):
+	def value(x):
+		return math.atan(1/x)
 	def der(x):
 		return -1/(1+x**2)
 	
-	output = ad.arccot(b)
-	assert output.value == math.arccot(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	output = ad.arccot(binv)
+	assert output.value == value(binv.value)
+	coef = der(binv.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
-def test_arccot_no_change(b):
-    b_d = b.derivatives
-    output = ad.arccot(b)
-    assert b_d == b.derivatives
+def test_arccot_no_change(binv):
+    b_d = binv.derivatives
+    output = ad.arccot(binv)
+    assert b_d == binv.derivatives
 	
 
 ###
 # arcsec
 ###
-def test_arcsec_s(s):
-	output = ad.arcsec(s)
-	assert output.value == math.arcsec(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+def test_arcsec_s(sinv):
+	def value(x):
+		return math.acos(1/x)
+	output = ad.arcsec(sinv)
+	assert output.value == value(sinv)
+	assert output.derivatives == {}
 
-def test_arcsec_d(b):
+def test_arcsec_d(binv):
+	def value(x):
+		return math.acos(1/x)
 	def der(x):
 		return 1/(math.abs(x)*math.sqrt(x**2-1))
 	
-	output = ad.arcsec(b)
-	assert output.value == math.arcsec(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	output = ad.arcsec(binv)
+	assert output.value == value(binv.value)
+	coef = der(binv.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
-def test_arcsec_no_change(b):
-    b_d = b.derivatives
-    output = ad.arcsec(b)
-    assert b_d == b.derivatives
+def test_arcsec_no_change(binv):
+    b_d = binv.derivatives
+    output = ad.arcsec(binv)
+    assert b_d == binv.derivatives
 	
 
 ###
 # arccsc
 ###
-def test_arccsc_s(s):
-	output = ad.arccsc(s)
-	assert output.value == math.arccsc(s)
-	assert output.derivatives == {'x':0,'y':0,'z':0}
+def test_arccsc_s(sinv):
+	def value(x):
+		return math.asin(1/x)
+	output = ad.arccsc(sinv)
+	assert output.value == value(sinv)
+	assert output.derivatives == {}
 
-def test_arccsc_d(b):
+def test_arccsc_d(binv):
+	def value(x):
+		return math.asin(1/x)
 	def der(x):
 		return -1/(math.abs(x)*math.sqrt(x**2-1))
 	
-	output = ad.arccsc(b)
-	assert output.value == math.arccsc(b.value)
-	assert output.derivatives == {'x':der(1.2), 'y':der(9.5), 'z':der(5)}
+	output = ad.arccsc(binv)
+	assert output.value == value(binv.value)
+	coef = der(binv.value)
+	assert output.derivatives == pytest.approx({'x':coef*1.2, 'y':coef*9.5, 'z':coef*5})
 	
-def test_arccsc_no_change(b):
-    b_d = b.derivatives
-    output = ad.arccsc(b)
-    assert b_d == b.derivatives
+def test_arccsc_no_change(binv):
+    b_d = binv.derivatives
+    output = ad.arccsc(binv)
+    assert b_d == binv.derivatives
 	
--- code/write_unary_tests.py
+++ code/write_unary_tests.py
@@ -1,31 +1,44 @@
 # write funciton name and 
+
+# name, value computation, derivative computation, argument
 test_tuples = [
-	("sin", "math.cos(x)"),
-	("cos", "-math.sin(x)"),
-	("tan", "math.sec(x)**2"),
-	("cot", "-math.csc(x)**2"),
-	("sec", "math.sec(x)*math.tan(x)"),
-	("csc", "-math.csc(x)*math.cot(x)"),
-	("arcsin", "1/math.sqrt(1-x**2)"),
-	("arccos", "-1/math.sqrt(1-x**2)"),
-	("arctan", "1/(1+x**2)"),
-	("arccot", "-1/(1+x**2)"),
-	("arcsec", "1/(math.abs(x)*math.sqrt(x**2-1))"),
-	("arccsc", "-1/(math.abs(x)*math.sqrt(x**2-1))")
+	("sin",		"math.sin(x)", 		"math.cos(x)", 							""),
+	("cos", 	"math.cos(x)", 		"-math.sin(x)", 						""),
+	("tan", 	"math.tan(x)", 		"1/(math.cos(x)**2)", 					""),
+	("cot", 	"1/math.tan(x)", 	"-1/(math.sin(x)**2)", 					""),
+	("sec", 	"1/math.cos(x)", 	"1/math.cos(x)*math.tan(x)", 			""),
+	("csc", 	"1/math.sin(x)", 	"-1/math.sin(x)*1/math.tan(x)", 		""),
+	("arcsin", 	"math.asin(x)", 	"1/math.sqrt(1-x**2)", 					""),
+	("arccos", 	"math.acos(x)", 	"-1/math.sqrt(1-x**2)", 				""),
+	("arctan", 	"math.atan(x)", 	"1/(1+x**2)", 							""),
+	("arccot", 	"math.atan(1/x)", 	"-1/(1+x**2)", 							"inv"),
+	("arcsec", 	"math.acos(1/x)", 	"1/(math.abs(x)*math.sqrt(x**2-1))", 	"inv"),
+	("arccsc", 	"math.asin(1/x)", 	"-1/(math.abs(x)*math.sqrt(x**2-1))", 	"inv")
 ]
 
 preamble = """import pytest
 import autodiff as ad
+import math
 
 @pytest.fixture
 def b():
     from autodiff import DualNumber
     b = DualNumber(None,-2/3,{'x':1.2, 'y':9.5, 'z':5})
     return b
+	
+@pytest.fixture
+def binv():
+    from autodiff import DualNumber
+    binv = DualNumber(None,-2/3,{'x':1.2, 'y':9.5, 'z':5})
+    return binv
 
 @pytest.fixture
 def s():
     return .42
+	
+@pytest.fixture
+def sinv():
+    return 1/.42
 """
 
 negation_test = """
@@ -49,23 +62,28 @@ test_pattern = """
 ###
 # {0}
 ###
-def test_{0}_s(s):
-	output = ad.{0}(s)
-	assert output.value == math.{0}(s)
-	assert output.derivatives == {{'x':0,'y':0,'z':0}}
-
-def test_{0}_d(b):
-	def der(x):
+def test_{0}_s(s{3}):
+	def value(x):
 		return {1}
+	output = ad.{0}(s{3})
+	assert output.value == value(s{3})
+	assert output.derivatives == {{}}
+
+def test_{0}_d(b{3}):
+	def value(x):
+		return {1}
+	def der(x):
+		return {2}
 	
-	output = ad.{0}(b)
-	assert output.value == math.{0}(b.value)
-	assert output.derivatives == {{'x':der(1.2), 'y':der(9.5), 'z':der(5)}}
+	output = ad.{0}(b{3})
+	assert output.value == value(b{3}.value)
+	coef = der(b{3}.value)
+	assert output.derivatives == pytest.approx({{'x':coef*1.2, 'y':coef*9.5, 'z':coef*5}})
 	
-def test_{0}_no_change(b):
-    b_d = b.derivatives
-    output = ad.{0}(b)
-    assert b_d == b.derivatives
+def test_{0}_no_change(b{3}):
+    b_d = b{3}.derivatives
+    output = ad.{0}(b{3})
+    assert b_d == b{3}.derivatives
 	
 """
 
@@ -73,4 +91,4 @@ with open("test_unary_functions.py",'w') as outfile:
 	outfile.write(preamble)
 	outfile.write(negation_test)
 	for cur_tup in test_tuples:
-		outfile.write(test_pattern.format(cur_tup[0],cur_tup[1]))
+		outfile.write(test_pattern.format(cur_tup[0],cur_tup[1], cur_tup[2], cur_tup[3]))
