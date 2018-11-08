7535f207c836481e119049bde4568a5347d0d1cf
erinwilliams32 <erinwilliams@g.harvard.edu>
2018-11-07 21:48:11 -0500

    updated init

--- code/__init__.py
+++ code/__init__.py
@@ -1,3 +1,7 @@
 # __init.py__
 
-from .autodiff import autodiff
+import autodiff.py
+import integration_test.py
+import test_binary_functions.py
+import test_unary_functions.py
+import write_unary_functions.py
