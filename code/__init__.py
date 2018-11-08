a1c0c9d1fccb4bbec56333095f6b5e49db88ec3a
erinwilliams32 <erinwilliams@g.harvard.edu>
2018-11-07 22:00:59 -0500

    Another update to init

--- code/__init__.py
+++ code/__init__.py
@@ -1,7 +1,7 @@
 # __init.py__
 
-from .code import autodiff
-from .code import integration_test
-from .code import test_binary_functions
-from .code import test_unary_functions
-from .code import write_unary_functions
+from code import autodiff
+from code import integration_test
+from code import test_binary_functions
+from code import test_unary_functions
+from code import write_unary_functions
