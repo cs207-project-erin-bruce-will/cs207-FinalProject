b391e6862a1b034594b6ed98f80d3cd2fb95e683
erinwilliams32 <erinwilliams@g.harvard.edu>
2018-12-11 12:03:27 -0500

    Updated __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,3 @@
 # __init__.py
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-name = "Autodiff_group3"
-=======
 name = 'AutoDiff_group3"
->>>>>>> 77d1001903f7939b66f421aedf582c99fa166ebd
-=======
->>>>>>> 500aa7f584967747e1c075ed29cd035f8d9c0d40
