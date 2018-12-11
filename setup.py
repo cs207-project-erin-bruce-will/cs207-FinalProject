0d0785fe76284cac5f705949976f3b42a9dceb3f
ErinWilliams32 <42841618+ErinWilliams32@users.noreply.github.com>
2018-12-11 11:44:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Autodiff_group3",
+    version="0.0.1",
+    author="Will Claybaugh, Fan (Bruce) Xiong, Erin Williams",
+    author_email="erinwilliams@g.harvard.edu",
+    description="Automatic differentiation with dual numbers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cs207-project-erin-bruce-will/cs207-FinalProject,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
