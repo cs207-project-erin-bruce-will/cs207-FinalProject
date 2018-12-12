c9b3f4180eac6c78ecea7548544bbad0270d3277
ErinWilliams32 <42841618+ErinWilliams32@users.noreply.github.com>
2018-12-11 23:31:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoDiff_group3",
-    version="0.0.5",
+    version="0.0.6",
     author="Will Claybaugh, Fan (Bruce) Xiong, Erin Williams",
     author_email="erinwilliams@g.harvard.edu",
     description="Automatic differentiation with dual numbers",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cs207-project-erin-bruce-will/cs207-FinalProject",
     packages=setuptools.find_packages(),
+    install_requires=['numpy','pytest']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
