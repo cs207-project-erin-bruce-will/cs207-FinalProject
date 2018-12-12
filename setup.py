f4d0dd1729e7920882d116469ca7a8ffe33f6112
ErinWilliams32 <42841618+ErinWilliams32@users.noreply.github.com>
2018-12-11 21:25:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cs207-project-erin-bruce-will/cs207-FinalProject",
-    packages=setuptools.find_packages(),
+    packages=['autodiff'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
